4eab769445e5ff9413bb342644b9050e62301c71
gonzalo <gonzaloibarra8@gmail.com>
2021-07-28 23:03:04 -0400

    actualizacion archivo views.py

--- appUno/views.py
+++ appUno/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render, HttpResponse, redirect
 from django.http import JsonResponse
-def index(request):
-    return HttpResponse("this is the equivalent of @app.route('/')!")
 def root(request):
     return redirect("/blogs")
 def index(request):
-    return HttpResponse('<h2>placeholder para luego mostrar una lista de todos los blogs</h2>')
+    return HttpResponse('<h2>egoplaceholder para lu mostrar una lista de todos los blogs</h2>')
 def new(request):
     return HttpResponse('<P>placeholder para mostrar un nuevo formulario para crear un nuevo blog <p>')
 def create(request):
